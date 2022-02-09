1c13578c5962e6e23fc52959332f50ce6ed0d9f5
Evgenken <politeh.evgen@gmail.com>
2022-02-09 20:35:13 +0300

    Практика с js

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,26 @@ let personalMovieDB = {
   genres: [],
   privat: false
 }
-const a = 'What the last film?',
-      b = 'How you like it film?',
-      c = prompt('What the last film?', ''),
-      d = prompt('How you like it film?', '');
- personalMovieDB.movie[a] = c;
- personalMovieDB.movie[b] = d;
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+  confirm('Просмотрено довольно мало фильмов')
+} else if (personalMovieDB.count <31) {
+  confirm('Вы классический зритель')
+} else if (personalMovieDB.count > 30) {
+  confirm('Вы киноман')
+} else {
+  confirm('Произошла ошибка')
+}
+ for (let i = 1; i < 3; i++) {
+   const a = prompt('What the last film?', ''),
+         b = prompt('How you like it film?', '');
+   if (b != null && a != null && b.length < 50 && a.length < 50 && b !== '' && a !== '') {
+     personalMovieDB.movie[a] = b;
+   } else {
+     i--;
+   }
+}
+ console.log(personalMovieDB);
+
 </script>
 
 <style>
